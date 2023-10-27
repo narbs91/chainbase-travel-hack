7b27d6a99003caf2306abb83a6ccce2eb685cf0b
Narbeh Shahnazarian <narbwork@gmail.com>
2023-10-26 17:56:57 -0700

    Adds PropertyService impl

--- src/app/types/property.ts
+++ src/app/types/property.ts
@@ -1,12 +1,13 @@
 export interface Property {
+    id: string
     name: string,
     address: string,
     price: number,
+    currency: string
     checkoutDate: string,
     checkInDate: string,
     imageUrl: string,
     description: string,
     listed: boolean,
     lister: string,
-    id: string
 }
