5971c9213de6c3a683b625520d2ed41c5406eb05
Narbeh Shahnazarian <narbwork@gmail.com>
2023-10-28 18:49:29 -0700

    touches up the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -14,16 +14,21 @@ import LoginForm from "./login-form";
 export default function LoginPage() {
   return (
     <PageLayout>
-      <Heading
-        bgClip="text"
-        bgGradient={"linear(135deg, #6699FF 0%, #FF3366 100%)"}
-      >
-        Login or create account
-      </Heading>
-      <Text color="black" textAlign={"center"}>
-        Creating an account will allow you to buy a hotel stay and let you list your own bookings for sale on the ReBookT marketplace!
-      </Text>
-      <Container maxW={{ base: "sm", sm: "xl" }} padding="20px">
+      <Container maxW={{ base: "sm", sm: "xl" }} textAlign={"center"}>
+        <Heading
+          bgClip="text"
+          bgGradient={"linear(135deg, #6699FF 0%, #FF3366 100%)"}
+          padding={4}
+        >
+          Login or Create an Account
+        </Heading>
+        <Text as='em' color="black">
+          Creating an account will allow you to buy a hotel stay and let you
+          list your own bookings for sale on the ReBookT marketplace!
+        </Text>
+      </Container>
+
+      <Container maxW={{ base: "sm", sm: "xl" }}>
         <LoginForm />
       </Container>
     </PageLayout>
